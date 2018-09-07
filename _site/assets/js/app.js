4474b7d865fa44f262da2c6551d9e0a9ab3e4b35
vela2018 <43020173+vela2018@users.noreply.github.com>
2018-09-06 23:08:49 -0500

    menu updates

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
+
+  // Clicking the Espanol button
   $("#span").click(function(){
     $(".span").removeClass("hide");
     $(".eng").addClass("hide");
     $(this).addClass("chosen");
     $("#eng").removeClass("chosen");
   });
+
+  // Clicking the English button
   $("#eng").click(function(){
     $(".eng").removeClass("hide");
     $(".span").addClass("hide");
     $(this).addClass("chosen");
     $("#span").removeClass("chosen");
   });
+
+  // Clicking the Menu button
+  $(".menu").click(function(){
+    $(".menu-overlay").fadeToggle(300);
+    $(".menu-close").toggleClass("hide");
+    $(".menu-open").toggleClass("hide");
+  });
+
 });
